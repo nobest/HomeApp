241cbd463ded03f01b4891629370fded746fa9f1
Semiu Hassan <semiuhassan86@gmail.com>
2025-05-28 08:32:44 +0100

    Make values constant

--- app/src/main/java/com/meticulous/homeapp/onboarding/OnboardingViewModel.kt
+++ app/src/main/java/com/meticulous/homeapp/onboarding/OnboardingViewModel.kt
@@ -16,9 +16,9 @@ class OnboardingViewModel : ViewModel() {
         get() = _currentDestination
 
     companion object {
-        val ONBOARDING_FIRST_STEP = 0
-        val ONBOARDING_SECOND_STEP = 1
-        val ONBOARDING_LAST_STEP = 2
+        const val ONBOARDING_FIRST_STEP = 0
+        const val ONBOARDING_SECOND_STEP = 1
+        const val ONBOARDING_LAST_STEP = 2
     }
 
     fun onFirstStepNextClicked() {
